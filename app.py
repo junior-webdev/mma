aabc65af64ce3ceee8b0052d4657bf58d5e2eafc
junior-webdev <junior-webdev@thep.live>
2020-02-01 22:25:09 +0200

    simplify

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
-def homepage():
-    return render_template('index.html')
+@app.route('/<name>')
+def homepage(name=None):
+    return render_template('index.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
