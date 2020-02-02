57a05a05262e2efac409fb3d748af04b39837498
junior-webdev <junior-webdev@thep.live>
2020-02-02 21:10:31 +0200

    more pages

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def homepage(name=None):
     return render_template('index1.html', name=name)
 
+@app.route('/hello')
+def hello_page():
+    return "Hello there"
+
+@app.route('/user/<user>')
+def user_page(user):
+    return "User: {0}".format(user)
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
